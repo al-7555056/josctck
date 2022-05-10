a36aed1b7d544808ad55fa9270ad833f8c863678
Haoqi Shan Xu <104140318+al-7555056@users.noreply.github.com>
2022-05-10 09:06:29 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,93 +1,46 @@
-radio.onReceivedValue(function (name, value) {
-    if (name == "Alan o atr") {
-        Alan_o_atr = value
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
-    if (name == "Atlante") {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, value)
+    if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
     }
-    if (name == "Atras") {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, value)
+    if (receivedNumber == 3) {
+        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     }
-    if (Alan_o_atr == 0) {
-        if (name == "izq") {
-            maqueen.motorStop(maqueen.Motors.M2)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, value)
-        }
-        if (name == "der") {
-            maqueen.motorStop(maqueen.Motors.M1)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, value)
-        }
-    } else {
-        if (name == "izq") {
-            maqueen.motorStop(maqueen.Motors.M2)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, value)
-        }
-        if (name == "der") {
-            maqueen.motorStop(maqueen.Motors.M1)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, value)
-        }
+    if (receivedNumber == 4) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
     }
-    if (name == "parar") {
+    if (receivedNumber == 0) {
         maqueen.motorStop(maqueen.Motors.All)
-    }
-    if (name == "luz iz") {
-        if (value == 1) {
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        } else {
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        }
-    }
-    if (name == "luz de") {
-        if (value == 1) {
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        } else {
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        }
-    }
-    if (name == "neopixel") {
-        if (value == 1) {
-            strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        } else {
-            strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        }
     }
 })
 let neopixel2 = 0
-let luz_de = 0
-let luz_iz = 0
-let Alan_o_atr = 0
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+let strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
 radio.setGroup(167)
 music.playMelody("C5 C5 C5 - - - - - ", 120)
 basic.forever(function () {
     if (CrocoKit_Input.Rocker(AnalogPin.P0, AnalogPin.P1, CrocoKit_Input.enRocker.Up)) {
-        radio.sendValue("Atlante", 255)
+        radio.sendNumber(1)
     } else if (CrocoKit_Input.Rocker(AnalogPin.P0, AnalogPin.P1, CrocoKit_Input.enRocker.Down)) {
-        radio.sendValue("Atras", 255)
+        radio.sendNumber(2)
     } else if (CrocoKit_Input.Rocker(AnalogPin.P0, AnalogPin.P1, CrocoKit_Input.enRocker.Left)) {
-        radio.sendValue("izq", 255)
+        radio.sendNumber(3)
     } else if (CrocoKit_Input.Rocker(AnalogPin.P0, AnalogPin.P1, CrocoKit_Input.enRocker.Right)) {
-        radio.sendValue("der", 255)
+        radio.sendNumber(4)
     } else {
-        radio.sendValue("parar", 0)
+        radio.sendNumber(0)
     }
     if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P3)) {
-        luz_iz += 1
-        if (luz_iz == 2) {
-            luz_iz = 0
-        }
-        radio.sendValue("luz iz", luz_iz)
+        radio.sendNumber(5)
     }
     if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P3)) {
-        luz_de += 1
-        if (luz_de == 2) {
-            luz_de = 0
-        }
-        radio.sendValue("luz de", luz_de)
+        radio.sendNumber(6)
     }
     if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P3)) {
-        radio.sendValue("Alan o atr", 0)
+        radio.sendNumber(7)
     }
     if (tinkercademy.ADKeyboard(ADKeys.D, AnalogPin.P3)) {
         neopixel2 += 1
@@ -97,6 +50,6 @@ basic.forever(function () {
         radio.sendValue("neopixel", neopixel2)
     }
     if (tinkercademy.ADKeyboard(ADKeys.E, AnalogPin.P3)) {
-        radio.sendValue("Alan o atr", 1)
+        radio.sendNumber(8)
     }
 })
